50bcc47c738f4132fd746924b80b5ef3c69c3c3f
bethq321 <bethqualls@comcast.net>
2024-09-10 10:18:25 -0500

    practice with if else

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
-let text = "Please visit Microsoft!";
-let result = text.split("");
+let userName = prompt("Who's there?", '');
+console.log(userName)
 
-console.log(text);
-console.log(result);
+if(userName === "Admin") {
+    let password = prompt("Password?", '');
+
+    if(password === "" || password === null) {
+        alert("Cancelled");
+    } else if(password === "TheMaster") {
+        alert("Welcome!");
+    } else {
+        alert("Wrong password");
+    };
+} else if(userName === "" || userName === null) {
+    alert("Cancelled");
+} else {
+    alert("I don't know who you are");
+}
