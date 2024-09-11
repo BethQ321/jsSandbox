27455032508f8906809529861e24dccf9ce8f565
bethq321 <bethqualls@comcast.net>
2024-09-11 09:37:36 -0500

    practice with functions

--- javascript.js
+++ javascript.js
@@ -1,16 +1,32 @@
-let answer = prompt("Input a number", '');
-
-if(answer < 0) {
-    alert(-1);
-} else if(answer > 0){
-    alert(1);
-} else {
-    alert(0);
+function add7(a) {
+    return a + 7;
 }
 
-let result = (a + b < 4) ? 'Below' : 'Over';
+let b = 5;
+console.log(b);
 
-let message = (login === 'Employee') ? 'Hello' :
-    (login === 'Director') ? 'Greetings':
-    (login === '') ? 'No login':
-    '';
+let c = add7(b);
+console.log(c);
+
+function multiply(a, b) {
+    return a*b;
+}
+
+let d = multiply(3, 2);
+console.log(d);
+
+function capitalize(word) {
+    let lowerWord = word.toLowerCase();
+    let capWord = lowerWord.charAt(0).toUpperCase() + lowerWord.slice(1);
+    return capWord;
+}
+
+let result = capitalize("jumPER");
+console.log(result);
+
+function lastLetter(string) {
+    return string.slice(-1);
+}
+
+let result2 = lastLetter("monkey");
+console.log(result2);
