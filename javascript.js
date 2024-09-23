39b4a0f60c0c5012c8030b0677d0d41a617527f8
bethq321 <bethqualls@comcast.net>
2024-09-23 14:53:13 -0500

    added event listeners

--- javascript.js
+++ javascript.js
@@ -1,34 +1,9 @@
-const container = document.querySelector("#container");
+//Groups of nodes
 
-const content = document.createElement("p");
-content.classList.add("content");
-content.textContent = "Hey I'm red!";
-content.style.color = "red";
+const buttons = document.querySelectorAll("button");
 
-container.appendChild(content);
-
-const moreContent = document.createElement("h3");
-moreContent.classList.add("moreContent");
-moreContent.textContent = "I'm a blue h3!";
-moreContent.style.color = "blue";
-
-container.appendChild(moreContent);
-
-const subContainer = document.createElement("div");
-subContainer.classList.add("subContainer");
-subContainer.style.border = "solid";
-subContainer.style.backgroundColor = "pink";
-
-const headline = document.createElement("h1");
-headline.classList.add("headline");
-headline.textContent = "I'm in a div";
-
-subContainer.appendChild(headline);
-
-const paragraph = document.createElement("p");
-// paragraph.classList.add("paragraph");
-paragraph.textContent = "ME TOO!";
-
-subContainer.appendChild(paragraph);
-
-container.appendChild(subContainer);
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
